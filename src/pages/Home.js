8bdd9fc6b4a173089f091e5d999ff6dc7c2ce82c
madan-kanala <mdnkanala.dev@gmail.com>
2022-08-29 16:42:26 +0600

    game assets design done in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AllCards from '../components/Home/Cards/AllCards';
 import Hero from '../components/Home/Hero/Hero';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <AllCards />
     </div>
   );
 };

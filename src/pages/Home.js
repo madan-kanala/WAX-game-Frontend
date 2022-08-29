c3290b2d91aadd813ee2a0f6082cfee35cf06446
madan-kanala <mdnkanala.dev@gmail.com>
2022-08-29 20:22:05 +0600

    add social links in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AllCards from '../components/Home/Cards/AllCards';
 import Hero from '../components/Home/Hero/Hero';
+import SocialLinks from '../components/Home/SocialLinks/SocialLinks';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <AllCards />
+      <SocialLinks />
     </div>
   );
 };

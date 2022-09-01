e5578ae606df5324bfce237e96de44bf68df6ac5
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-02 00:33:06 +0600

    udpate consignment page

--- src/components/Hustle/Consignment/Content/MiddleContent.js
+++ src/components/Hustle/Consignment/Content/MiddleContent.js
@@ -5,17 +5,17 @@ const MiddleContent = () => {
     <div className='md:w-4/12 w-full order-1 mb-5 md:mb-0 md:order-2 '>
       <div className='mt-5 flex justify-center items-center'>
         <div className='xl:w-1/2'>
-          <p className='font-semibold text-base text-olive mb-1 text-center'>
-            Collected Point
+          <p className='font-semibold text-lg text-olive mb-1 text-center'>
+            Enter Amount
           </p>
-          <button className='bg-red-700 px-5 mb-3 py-2.5 rounded-md block w-full'>
+          <button className='bg-red-700 px-5 mb-4 py-2.5 rounded-md block w-full'>
             1000 HDC
           </button>
-          <p className='font-semibold text-base  text-center'>
-            Collected Point
+          <p className='font-semibold text-lg text-olive text-center mt-5 mb-2'>
+            Rate 250HWT = 10 HDC
           </p>
-          <button className='bg-blue-500 px-5 py-2.5 rounded-md block w-full'>
-            Collect Rewards
+          <button className='bg-money-green px-5 py-2.5 rounded-md block w-full'>
+            Add Work
           </button>
         </div>
       </div>

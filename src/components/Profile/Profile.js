babd2dd9301f9cf0e6feab28314e3df79354555e
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-01 11:06:13 -0400

    add profile top image

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Information from './Information';
-import ProfileTable from './ProfileTable';
+import React from "react";
+import image1 from "../../assets/images/profile/profile v2.png";
+import Information from "./Information";
+import ProfileTable from "./ProfileTable";
 
 const Profile = () => {
   return (
-    <div className=''>
-      <div className='md:flex'>
+    <div className="">
+      <div className="img flex justify-center">
+        <img src={image1} alt="" />
+      </div>
+      <div className="md:flex">
         <Information />
         <ProfileTable />
       </div>

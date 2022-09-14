f8930077c0bdbc2ec3d7a157d970b25cae4a2986
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-14 17:08:00 +0600

    profile stake card design done

---
+++ src/components/Profile/LeftSide.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Information from './Information';
+import StakeCard from './StakeCard';
+
+const LeftSide = () => {
+  return (
+    <>
+      <div className='md:w-4/12 mb-4 md:mb-0'>
+        <Information />
+        <StakeCard />
+      </div>
+    </>
+  );
+};
+
+export default LeftSide;

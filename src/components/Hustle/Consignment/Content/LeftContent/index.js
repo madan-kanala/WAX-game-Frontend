b6e64c48f7c3e907f819353b2bf72f5159fc4cd2
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-27 00:35:48 +0600

    style and color updated in consignment page

--- src/components/Hustle/Consignment/Content/LeftContent/index.js
+++ src/components/Hustle/Consignment/Content/LeftContent/index.js
@@ -8,9 +8,9 @@ const LeftContent = () => {
         <h2 className='text-left text-olive text-xl mb-1 font-bold'>
           Transporter
         </h2>
-        <div className='text-center box-border py-8 px-3 border rounded-md bg-olive h-80 overflow-y-auto overflow-x-hidden'>
+        <div className='text-center box-border py-8 px-3 border rounded-md h-80 overflow-y-auto overflow-x-hidden p-8 bg-black mr-2 custom-shadow '>
           <div className='flex justify-center gap-4 flex-wrap'>
-            {[1, 2].map((i) => (
+            {[1].map((i) => (
               <Character key={i} />
             ))}
           </div>

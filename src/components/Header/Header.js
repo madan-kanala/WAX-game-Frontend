1847e08a0afb06d8db47ff6ef0b352ce3450e66d
madan-kanala <mdnkanala.dev@gmail.com>
2022-10-04 12:20:51 +0600

    header mneu fixed at top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import usePageOffset from '../../hooks/usePageOffset';
 import AuthMenu from './AuthMenu';
 import HumbuggerMenu from './Humberger/HumbuggerMenu';
 import Logo from './Logo';
 import Menu from './Menu';
 
 const Header = ({ isTransparent = false }) => {
-  const { y: pageYOffset } = usePageOffset();
   return (
     <div
-      className={`${isTransparent ? 'bg-transparent' : 'bg-black'} ${
-        pageYOffset > 100 ? 'fixed w-full top-0' : 'relative'
-      } z-50 custom-shadow`}
+      className={`${
+        isTransparent ? 'bg-transparent' : 'bg-black'
+      } z-50 custom-shadow fixed w-full top-0`}
     >
       <div className='mx-10'>
         <div className='flex justify-between items-center'>

9cc4e14d6f4ab4dffb207d73ba49e1c53213515f
madan-kanala <mdnkanala.dev@gmail.com>
2022-10-05 13:04:38 +0600

    profile stats update

--- src/components/SafeHouse/Information.js
+++ src/components/SafeHouse/Information.js
@@ -8,41 +8,54 @@ const Information = () => {
     >
       <div className=''>
         <div className='flex flex-col gap-y-1 '>
-          <p className='text-olive'>
-            <span className='text-olive font-bold'>Name:</span> Jhon Doe
-          </p>
-          <p className='text-olive'>
-            <span className='text-olive font-bold'>Gender:</span> Male
-          </p>
-          <p className='text-olive'>
-            <span className='text-olive font-bold'>G-Rank:</span> 3231
-          </p>
+          <div className='text-olive'>
+            <div className='flex gap-1 box-border items-center gap-x-2'>
+              <p>Name:</p>
+              <input
+                type='text'
+                name=''
+                id=''
+                placeholder='enter name'
+                className='border rounded-md text-base px-2 py-0.5 box-border block w-full bg-transparent'
+              />
+            </div>
+            <div className='flex gap-1 box-border items-center gap-x-2 my-3'>
+              <p>Gang:</p>
+              <input
+                type='text'
+                name=''
+                id=''
+                placeholder='enter name'
+                className='border rounded-md text-base px-2 py-0.5 box-border block w-full bg-transparent'
+              />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className='my-4'>
-        <span className='text-olive font-bold'> Stats: </span>
-        <div className='text-base h-12'>
-          {/* Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Consequatur temporibus voluptates numquam fugiat labore alias eum,
-            suscipit provident, sequi nam asperiores obcaecati veritatis
-            quibusdam quod quas similique accusantium facilis est! */}
+      <div>
+        <div className='my-4'>
+          <span className='text-olive font-bold'> Stats: </span>
         </div>
-      </div>
-      <div className='mt-2'>
-        <p className='text-olive'>
-          <span className='text-olive font-bold'> Total successful drops:</span>{' '}
-          0
-        </p>
-        <p className='text-olive'>
-          <span className='text-olive font-bold'> Time spent in jail:</span> 0
-        </p>
-        <p className='text-olive'>
-          <span className='text-olive font-bold'> successful invasions:</span> 0
-        </p>
+        <div className='mt-2'>
+          <Item text={'G Rank'} value={'0'} />
+          <Item text={'Total Bonus Reward'} value={'1%'} />
+          <Item text={'Dirty Cash per hour'} value={'0'} />
+          <Item text={'Dirty Cash per hour'} value={' 10.5'} />
+          <Item text={'Clean Cash per day'} value={'5'} />
+          <Item text={' Hustlers Token per day'} value={'.75'} />
+          <Item text={'Total Stop & Search'} value={'60'} />
+          <Item text={'Total Successful Drops'} value={'5'} />
+          <Item text={'Total Drop Intercepted'} value={'3'} />
+        </div>
       </div>
     </div>
   );
 };
+
+const Item = ({ text, value }) => (
+  <p className='text-olive'>
+    <span className='text-olive '>{text}:</span> {'  '} {value}
+  </p>
+);
 
 export default Information;

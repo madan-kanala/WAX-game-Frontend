6bbfbe8af8bbdeb2e6a9c385987ee997b24e43f9
madan-kanala <mdnkanala.dev@gmail.com>
2022-10-09 20:45:03 +0600

    header login menu fixed with new design

--- src/components/Header/User/AuthUserMenu.js
+++ src/components/Header/User/AuthUserMenu.js
@@ -2,19 +2,15 @@ import React from 'react';
 import icon2 from '../../../assets/images/icons/clean-cash.png';
 import icon3 from '../../../assets/images/icons/hst-final.png';
 import icon1 from '../../../assets/images/icons/hustler-clean-cash-token.png';
-import icon4 from '../../../assets/images/icons/hwt.png';
 const AuthUserMenu = () => {
   return (
     <div className='text-primary py-3'>
       <div>
-        <p className='btn btn-default rounded-md'>Thedragon.gm</p>
+        <p className=' text-center'>Thedragon.gm</p>
         <div className='flex gap-x-2'>
           <Item icon={icon1} text='201' />
           <Item icon={icon2} text='201' />
-        </div>
-        <div className='flex gap-x-2'>
           <Item icon={icon3} text='201' />
-          <Item icon={icon4} text='201' />
         </div>
       </div>
     </div>
